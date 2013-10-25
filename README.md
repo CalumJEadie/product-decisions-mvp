33368d51fd53db7f08476f45393b88c1a216556a
Calum J. Eadie <calum@calumjeadie.com>
2013-10-25 18:06:08 +0100

    Added postgres to dev setup

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ product-decisions-mvp
 
 http://product-decisions-mvp.herokuapp.com/
 
+Resources
+---------
+
 http://www.ebay.co.uk/itm/FALCON-58CM-VINTAGE-ROAD-BIKE-REYNOLDS-531-5-SPEED-PURPLE-/300997251850
 
 https://github.com/angular/angular-seed
+
+https://devcenter.heroku.com/articles/heroku-postgresql
 
 Local Development
 -----------------
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Install core Python development environment, from there on
-# can use pip and virtual environments.
+# Install core Python/Django/Postgres development environment, from ther
+# on can use pip and virtual environments.
 apt-get update
-apt-get install -y python-pip vim libpq-dev python-dev
+apt-get install -y python-pip vim libpq-dev python-dev postgresql
 pip install virtualenv
 
 # Install Heroku.
