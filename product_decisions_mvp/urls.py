f15666794da84c181f9169b19a31c83c66e87617
Calum J. Eadie <calum@calumjeadie.com>
2013-10-28 16:22:16 +0000

    Updated links to remove hard coded urls

--- product_decisions_mvp/urls.py
+++ product_decisions_mvp/urls.py
@@ -18,9 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
 
-    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
 
     # e.g. /bikes/1/
     url(r'^bikes/(?P<bike_id>\d+)/$', views.detail, name='detail'),
